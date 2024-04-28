57a83edc10163901d76e4bf7b051e6ff7a6949eb
lipengyi <pengyi1@staff.weibo.com>
2024-04-28 19:47:34 +0800

    features: add enum for raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -28,9 +28,15 @@ contract Raffle {
     error Raffle__SendMoreToEnterRaffle();
     error Raffle__TransferFailed();
 
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
     uint256 immutable i_entranceFee;
     address payable[] private s_players;
     address private s_recentWinner;
+    RaffleState private s_raffleState;
 
     event RaffleEnter(address indexed player);
     event WinnerPicked(address indexed player);
@@ -39,13 +45,18 @@ contract Raffle {
         uint256 entranceFee
     ){
         i_entranceFee = entranceFee;
+        s_raffleState = RaffleState.OPEN;
     }
 
     function enterRaffle() public payable {
         if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
         }
+        if(s_raffleState != RaffleState.OPEN) {
+            revert();
+        }
         s_players.push(payable(msg.sender));
+
         emit RaffleEnter(msg.sender);
     }
 
@@ -53,6 +64,7 @@ contract Raffle {
         address payable recentWinner = s_players[index];
         s_recentWinner = recentWinner;
         s_players = new address payable[](0);
+        s_raffleState = RaffleState.OPEN;
         emit WinnerPicked(recentWinner);
 
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
